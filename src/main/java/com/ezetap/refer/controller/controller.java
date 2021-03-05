9439293cc274b4f073353b2306a62b0a595e822f
Sushil <sushilkumar96176@gmail.com>
2021-03-05 15:16:21 +0530

    Second

--- src/main/java/com/ezetap/refer/controller/controller.java
+++ src/main/java/com/ezetap/refer/controller/controller.java
@@ -1,4 +1,13 @@
 package com.ezetap.refer.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class controller {
+
+    @GetMapping("/home")
+    public String home() {
+        return "Hello World";
+    }
 }
