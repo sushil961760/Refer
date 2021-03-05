f4240a75d6b300b28e5db75aa98984b8bebc43c2
Sushil <sushilkumar96176@gmail.com>
2021-03-05 14:28:54 +0530

    First

---
+++ src/main/java/com/ezetap/refer/ReferApplication.java
@@ -0,0 +1,13 @@
+package com.ezetap.refer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ezetap/refer/ReferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezetap.refer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
